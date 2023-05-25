2edb21229cfd622b19894b91a153cea2ffc2affb
Marci260 <marcikanya@gmail.com>
2023-05-25 19:14:07 +0200

    Add project files.

---
+++ EDVC1J_HFT_2022232.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EDVC1J_HFT_2022232.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ EDVC1J_HFT_2022232.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EDVC1J_HFT_2022232.Models
+{
+    public class Class1
+    {
+    }
+}
