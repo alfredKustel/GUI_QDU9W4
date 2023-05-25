4d36f7ec1065a128cd544ba2a004f7a873422438
Marci260 <marcikanya@gmail.com>
2023-05-25 19:35:32 +0200

    created dbContext

---
+++ EDVC1J_HFT_2022232.Data/RestaurantDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EDVC1J_HFT_2022232.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EDVC1J_HFT_2022232.Data
+{
+    public class RestaurantDbContext : DbContext
+    {
+        public DbSet<Chef> Chefs { get; set; }
+        public DbSet<Receipt> Receipts { get; set; }
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+        public RestaurantDbContext()
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
