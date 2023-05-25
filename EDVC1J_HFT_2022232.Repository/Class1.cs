4a9f1888d464629068de7886fce6f79ac0b10746
Marci260 <marcikanya@gmail.com>
2023-05-25 19:29:03 +0200

    created repository interfaces

---
+++ EDVC1J_HFT_2022232.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EDVC1J_HFT_2022232.Repository
+{
+    public class Class1
+    {
+    }
+}
---
+++ EDVC1J_HFT_2022232.Repository/IChefRepository.cs
@@ -0,0 +1,18 @@
+﻿using EDVC1J_HFT_2022232.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDVC1J_HFT_2022232.Repository
+{
+    public interface IChefRepository
+    {
+        void Create(Chef chef);
+        void Delete(int id);
+        IQueryable<Chef> GetAll();
+        Chef Read(int id);
+        void Update(Chef chef);
+    }
+}
