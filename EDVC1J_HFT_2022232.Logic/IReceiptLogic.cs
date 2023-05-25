5d53c5bb16214a840e295c1f18d0bb0ab434e4a1
Marci260 <marcikanya@gmail.com>
2023-05-25 19:22:47 +0200

    logic interfaces created

---
+++ EDVC1J_HFT_2022232.Logic/IReceiptLogic.cs
@@ -0,0 +1,20 @@
+﻿using EDVC1J_HFT_2022232.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDVC1J_HFT_2022232.Logic
+{
+    public interface IReceiptLogic
+    {
+        void Create(Receipt receipt);
+        void Delete(int id);
+        IEnumerable<Receipt> FrancoDeMilanReceipts();
+        IQueryable<Receipt> GetAll();
+        IEnumerable<Receipt> PeepReceipts();
+        Receipt Read(int id);
+        void Update(Receipt receipt);
+    }
+}
