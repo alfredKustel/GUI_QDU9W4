35ef43088e8684d8abd9305c9af7c157ddc1998a
Marci260 <marcikanya@gmail.com>
2023-05-25 19:40:43 +0200

    set onmodelCreating

--- EDVC1J_HFT_2022232.Data/RestaurantDbContext.cs
+++ EDVC1J_HFT_2022232.Data/RestaurantDbContext.cs
@@ -22,5 +22,53 @@ namespace EDVC1J_HFT_2022232.Data
                                 |DataDirectory|\RestaurantDb.mdf;
                                 Integrated Security=True");
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Restaurant>(entity =>
+            {
+                entity
+                    .HasMany(restaurant => restaurant.WorkingChefs)
+                    .WithOne(workingchef => workingchef.Restaurant)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+            modelBuilder.Entity<Restaurant>(entity =>
+            {
+                entity
+                    .HasMany(restaurant => restaurant.Menu)
+                    .WithOne(receipts => receipts.Restaurant)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+            modelBuilder.Entity<Chef>(entity =>
+            {
+                entity
+                    .HasOne(chef => chef.Restaurant)
+                    .WithMany(restaurant => restaurant.WorkingChefs)
+                    .HasForeignKey(chef => chef.RestaurantID)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+            modelBuilder.Entity<Chef>(entity =>
+            {
+                entity
+                    .HasMany(chef => chef.Specialities)
+                    .WithOne(receipt => receipt.Chef)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+            modelBuilder.Entity<Receipt>(entity =>
+            {
+                entity
+                    .HasOne(receipt => receipt.Restaurant)
+                    .WithMany(restaurant => restaurant.Menu)
+                    .HasForeignKey(receipt => receipt.RestaurantID)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+            modelBuilder.Entity<Receipt>(entity =>
+            {
+                entity
+                    .HasOne(receipt => receipt.Chef)
+                    .WithMany(chef => chef.Specialities)
+                    .HasForeignKey(receipt => receipt.ChefID)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+        }
     }
 }
