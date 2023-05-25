b86ba8f89bca43324f421f43b025e7b6757dec07
Marci260 <marcikanya@gmail.com>
2023-05-25 19:54:33 +0200

    restaurant controller created

---
+++ EDVC1J_HFT_2022232.Endpoint/Controllers/RestaurantController.cs
@@ -0,0 +1,55 @@
+﻿using EDVC1J_HFT_2022232.Logic;
+using EDVC1J_HFT_2022232.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EDVC1J_HFT_2022232.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RestaurantController : ControllerBase
+    {
+        IRestaurantLogic restaurantLogic;
+        public RestaurantController(IRestaurantLogic restaurantLogic)
+        {
+            this.restaurantLogic = restaurantLogic;
+        }
+        // GET: /restaurant
+        [HttpGet]
+        public IEnumerable<Restaurant> Get()
+        {
+            return restaurantLogic.GetAll();
+        }
+
+        // GET /restaurant/id
+        [HttpGet("{id}")]
+        public Restaurant Get(int id)
+        {
+            return restaurantLogic.Read(id);
+        }
+
+        // POST /restaurant
+        [HttpPost]
+        public void Post([FromBody] Restaurant value)
+        {
+            restaurantLogic.Create(value);
+        }
+
+        // PUT /restaurant
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Restaurant value)
+        {
+            restaurantLogic.Update(value);
+        }
+
+        // DELETE /restaurant/id
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            restaurantLogic.Delete(id);
+        }
+    }
+}
