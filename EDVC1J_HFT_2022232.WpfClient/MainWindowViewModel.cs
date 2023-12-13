247a02333686c8308949b6d869a5cd89705f1f7e
Marci260 <marcikanya@gmail.com>
2023-12-13 11:50:21 +0100

    init all crud command, mainwindow extend

--- EDVC1J_HFT_2022232.WpfClient/MainWindowViewModel.cs
+++ EDVC1J_HFT_2022232.WpfClient/MainWindowViewModel.cs
@@ -17,6 +17,10 @@ namespace EDVC1J_HFT_2022232.WpfClient
     {
         public RestCollection<Chef> Chefs { get; set; }
 
+        public RestCollection<Restaurant> Restaurants { get; set; }
+
+        public RestCollection<Receipt> Receipts { get; set; }
+
         private Chef selectedChef;
 
         public Chef SelectedChef
@@ -25,6 +29,30 @@ namespace EDVC1J_HFT_2022232.WpfClient
             set {
                 SetProperty(ref selectedChef, value);
                 (DeleteChefCommand as RelayCommand).NotifyCanExecuteChanged();
+            }
+        }
+
+        private Restaurant selectedRestaurant;
+
+        public Restaurant SelectedRestaurant
+        {
+            get { return selectedRestaurant; }
+            set
+            {
+                SetProperty(ref selectedRestaurant, value);
+                (DeleteRestaurantCommand as RelayCommand).NotifyCanExecuteChanged();
+            }
+        }
+
+        private Receipt selectedReceipt;
+
+        public Receipt SelectedReceipt
+        {
+            get { return selectedReceipt; }
+            set
+            {
+                SetProperty(ref selectedReceipt, value);
+                (DeleteRestaurantCommand as RelayCommand).NotifyCanExecuteChanged();
             }
         }
 
@@ -41,6 +69,14 @@ namespace EDVC1J_HFT_2022232.WpfClient
         public ICommand DeleteChefCommand { get; set; }
         public ICommand UpdateChefCommand { get; set; }
 
+        public ICommand CreateRestaurantCommand { get; set; }
+        public ICommand DeleteRestaurantCommand { get; set; }
+        public ICommand UpdateRestaurantCommand { get; set; }
+
+        public ICommand CreateReceiptCommand { get; set; }
+        public ICommand DeleteReceiptCommand { get; set; }
+        public ICommand UpdateReceiptCommand { get; set; }
+
         public static bool IsInDesignMode
         {
             get
@@ -53,10 +89,13 @@ namespace EDVC1J_HFT_2022232.WpfClient
         public MainWindowViewModel()
         {
 
-            Chefs = new RestCollection<Chef>("http://localhost:49326/", "restaurant");
+            Chefs = new RestCollection<Chef>("http://localhost:49326/", "chef");
+            Restaurants = new RestCollection<Restaurant>("http://localhost:49326/", "restaurant");
+            Receipts = new RestCollection<Receipt>("http://localhost:49326/", "receipt");
 
             if (!IsInDesignMode)
             {
+                //Chefs
                 CreateChefCommand = new RelayCommand(() =>
                 {
                     Chefs.Add(new Chef()
@@ -88,6 +127,70 @@ namespace EDVC1J_HFT_2022232.WpfClient
                 });
                 SelectedChef = new Chef();
 
+                //Restaurants
+                CreateRestaurantCommand = new RelayCommand(() =>
+                {
+                    Restaurants.Add(new Restaurant()
+                    {
+                        Name = SelectedRestaurant.Name
+                    });
+                });
+
+
+                UpdateRestaurantCommand = new RelayCommand(() =>
+                {
+                    try
+                    {
+                        Restaurants.Update(SelectedRestaurant);
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        ErrorMessage = ex.Message;
+                    }
+                });
+
+                DeleteRestaurantCommand = new RelayCommand(() =>
+                {
+                    Restaurants.Delete(SelectedRestaurant.ID);
+                },
+                () =>
+                {
+                    return SelectedRestaurant != null;
+                });
+                SelectedRestaurant = new Restaurant();
+
+                //Receipts
+
+                CreateReceiptCommand = new RelayCommand(() =>
+                {
+                    Receipts.Add(new Receipt()
+                    {
+                        Name = SelectedReceipt.Name
+                    });
+                });
+
+
+                UpdateReceiptCommand = new RelayCommand(() =>
+                {
+                    try
+                    {
+                        Receipts.Update(SelectedReceipt);
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        ErrorMessage = ex.Message;
+                    }
+                });
+
+                DeleteReceiptCommand = new RelayCommand(() =>
+                {
+                    Receipts.Delete(SelectedReceipt.ID);
+                },
+                () =>
+                {
+                    return SelectedReceipt != null;
+                });
+                SelectedReceipt = new Receipt();
             }
         }
     }
