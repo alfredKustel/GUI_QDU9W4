bea7107480a62fdbb84dafa0619c1ec9c35f170d
Marci260 <marcikanya@gmail.com>
2023-12-13 10:16:42 +0100

    init restcollection, and wof client

---
+++ EDVC1J_HFT_2022232.WpfClient/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using EDVC1J_HFT_2022232.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static EDVC1J_HFT_2022232.WpfClient.Restcollection;
+
+namespace EDVC1J_HFT_2022232.WpfClient
+{
+    public class MainWindowViewModel
+    {
+        public RestCollection<Chef> Chefs { get; set; }
+
+        public MainWindowViewModel() 
+        {
+            Chefs = new RestCollection<Chef>("http://localhost:49326","chef");
+        }
+    }
+}
