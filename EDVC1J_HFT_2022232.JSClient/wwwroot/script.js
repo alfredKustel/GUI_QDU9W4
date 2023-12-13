2da2c25f8ba26d2a29b64bf796623b6987a018ef
Marci260 <marcikanya@gmail.com>
2023-12-13 18:56:29 +0100

    json crud methods done

--- EDVC1J_HFT_2022232.JSClient/wwwroot/script.js
+++ EDVC1J_HFT_2022232.JSClient/wwwroot/script.js
@@ -1,8 +1,12 @@
 ﻿
 let chefs = [];
+let restaurants = [];
+let Receipts = [];
 let connection = null;
 
-let chefidtoupdate = -1
+let chefidtoupdate = -1;
+let restaurantidtoupdate = -1;
+let receipttoupdate = -1;
 
 getdata();
 setupSignalR();
@@ -19,6 +23,10 @@ function setupSignalR() {
     });
 
     connection.on("ChefDeleted", (user, message) => {
+        getdata();
+    });
+
+    connection.on("ChefUpdated", (user, message) => {
         getdata();
     });
 
@@ -43,46 +51,146 @@ async function start() {
 };
 
 async function getdata() {
+    await fetch('http://localhost:49326/restaurant')
+        .then(x => x.json())
+        .then(y => {
+            restaurants = y;
+            console.log(restaurants);
+            //window.onload();
+            displayrestaurant();
+        });
+
     await fetch('http://localhost:49326/chef')
         .then(x => x.json())
         .then(y => {
             chefs = y;
-            //console.log(chefs);
+            console.log(chefs);
             //window.onload();
-            display();
+            displaychef();
         });
+
+    await fetch('http://localhost:49326/receipt')
+        .then(x => x.json())
+        .then(y => {
+            receipts = y;
+            console.log(receipts);
+            //window.onload();
+            displayreceipts();
+        });
+
+
 }
 
-function display() {
-    document.getElementById('resultarea').innerHTML = "";
+function displaychef() {
+    document.getElementById('chefresultarea').innerHTML = "";
     chefs.forEach(t => {
-        document.getElementById('resultarea').innerHTML +=
+        document.getElementById('chefresultarea').innerHTML +=
             "<tr><td>" + t.id + "</td><td>"
             + t.name + "</td><td>" +
                 `<button type="button" onclick="removechef(${t.id})">Delete</button>`
             +
-             `<button type="button" onclick="showupdate(${t.id})">Update</button>`
+             `<button type="button" onclick="showchefpdate(${t.id})">Update</button>`
             +
             "</td></tr>";
     });
 }
 
-function showupdate(id)
+function displayrestaurant() {
+    document.getElementById('restaurantresultarea').innerHTML = "";
+    restaurants.forEach(t => {
+        document.getElementById('restaurantresultarea').innerHTML +=
+            "<tr><td>" + t.id + "</td><td>"
+            + t.name + "</td><td>" +
+            `<button type="button" onclick="removrestaurant(${t.id})">Delete</button>`
+            +
+            `<button type="button" onclick="showrestaurantupdate(${t.id})">Update</button>`
+            +
+            "</td></tr>";
+    });
+}
+
+function displayreceipts() {
+    document.getElementById('receipttresultarea').innerHTML = "";
+    receipts.forEach(t => {
+        document.getElementById('receipttresultarea').innerHTML +=
+            "<tr><td>" + t.id + "</td><td>"
+            + t.name + "</td><td>" +
+            `<button type="button" onclick="removereceipt(${t.id})">Delete</button>`
+            +
+            `<button type="button" onclick="showreceiptupdate(${t.id})">Update</button>`
+            +
+            "</td></tr>";
+    });
+}
+
+function showchefpdate(id)
 {
     document.getElementById('chefnametoupdate').value = chefs.find(t => t['id'] == id)['name'];
-    document.getElementById('updateformdiv').style.display = 'flex';
+    document.getElementById('updatechefformdiv').style.display = 'flex';
     chefidtoupdate = id;
 
 }
 
-function update() {
-    document.getElementById('updateformdiv').style.display = 'none';
+function cehfupdate() {
+    document.getElementById('updatechefformdiv').style.display = 'none';
     let name = document.getElementById('chefnametoupdate').value;
     fetch('http://localhost:49326/chef', {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json', },
         body: JSON.stringify(
-            { Name: name , ID : chefidtoupdate})
+            { Name: name, ID: chefidtoupdate })
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
+
+}
+
+function showrestaurantupdate(id) {
+    document.getElementById('restaurantnametoupdate').value = restaurants.find(t => t['id'] == id)['name'];
+    document.getElementById('updaterestaurantformdiv').style.display = 'flex';
+    restaurantidtoupdate = id;
+
+}
+
+function showreceiptupdate(id) {
+    document.getElementById('receiptnametoupdate').value = receipts.find(t => t['id'] == id)['name'];
+    document.getElementById('updatereceiptformdiv').style.display = 'flex';
+    receiptidtoupdate = id;
+
+}
+
+
+
+function restaurantupdate() {
+    document.getElementById('updaterestaurantformdiv').style.display = 'none';
+    let name = document.getElementById('restaurantnametoupdate').value;
+    fetch('http://localhost:49326/restaurant', {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            { Name: name, ID: restaurantidtoupdate })
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
+
+}
+
+function receiptupdate() {
+    document.getElementById('updatereceiptformdiv').style.display = 'none';
+    let name = document.getElementById('receiptnametoupdate').value;
+    fetch('http://localhost:49326/receipt', {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            { Name: name, ID: receiptidtoupdate })
     })
         .then(response => response)
         .then(data => {
@@ -108,9 +216,73 @@ function removechef(id) {
 
 }
 
-function create() {
+function removereceipt(id) {
+    fetch('http://localhost:49326/receipt/' + id, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json', },
+        body: null
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
+
+}
+
+function removrestaurant(id) {
+    fetch('http://localhost:49326/restaurant/' + id, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json', },
+        body: null
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
+
+}
+
+function chefcreate() {
     let name = document.getElementById('chefname').value;
     fetch('http://localhost:49326/chef', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            { Name: name })
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
+
+}
+
+function restaurantcreate() {
+    let name = document.getElementById('restaurantname').value;
+    fetch('http://localhost:49326/restaurant', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            { Name: name })
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
+
+}
+
+function receiptcreate() {
+    let name = document.getElementById('receiptname').value;
+    fetch('http://localhost:49326/receipt', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json', },
         body: JSON.stringify(
