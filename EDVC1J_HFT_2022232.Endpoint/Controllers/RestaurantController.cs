fec1668003b5870a3f1da356bc948e37f43a9d30
Marci260 <marcikanya@gmail.com>
2023-12-13 17:50:18 +0100

    some signalR fixes, and additions

--- EDVC1J_HFT_2022232.Endpoint/Controllers/RestaurantController.cs
+++ EDVC1J_HFT_2022232.Endpoint/Controllers/RestaurantController.cs
@@ -1,6 +1,8 @@
-﻿using EDVC1J_HFT_2022232.Logic;
+﻿using EDVC1J_HFT_2022232.Endpoint.services;
+using EDVC1J_HFT_2022232.Logic;
 using EDVC1J_HFT_2022232.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,9 +17,11 @@ namespace EDVC1J_HFT_2022232.Endpoint.Controllers
     public class RestaurantController : ControllerBase
     {
         IRestaurantLogic restaurantLogic;
-        public RestaurantController(IRestaurantLogic restaurantLogic)
+        IHubContext<SignalRHub> hub;
+        public RestaurantController(IRestaurantLogic restaurantLogic, IHubContext<SignalRHub> hub)
         {
             this.restaurantLogic = restaurantLogic;
+            this.hub = hub;
         }
         // GET: /restaurant
         [HttpGet]
@@ -38,20 +42,24 @@ namespace EDVC1J_HFT_2022232.Endpoint.Controllers
         public void Post([FromBody] Restaurant value)
         {
             restaurantLogic.Create(value);
+            this.hub.Clients.All.SendAsync("RestaurantCreated", value);
         }
 
         // PUT /restaurant
-        [HttpPut("{id}")]
+        [HttpPut]
         public void Put(int id, [FromBody] Restaurant value)
         {
             restaurantLogic.Update(value);
+            this.hub.Clients.All.SendAsync("RestaurantUpdated", value);
         }
 
         // DELETE /restaurant/id
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var receiptToDelete = this.restaurantLogic.Read(id);
             restaurantLogic.Delete(id);
+            this.hub.Clients.All.SendAsync("RestaurantDeleted", receiptToDelete);
         }
     }
 }
