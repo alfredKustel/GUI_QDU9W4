8f8904e69ce51056eaadc565ad7d4a91adaa643c
Marci260 <marcikanya@gmail.com>
2023-12-13 10:33:50 +0100

    mainwindow commands

--- EDVC1J_HFT_2022232.WpfClient/MainWindowViewModel.cs
+++ EDVC1J_HFT_2022232.WpfClient/MainWindowViewModel.cs
@@ -1,20 +1,56 @@
 ﻿using EDVC1J_HFT_2022232.Models;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using static EDVC1J_HFT_2022232.WpfClient.Restcollection;
 
 namespace EDVC1J_HFT_2022232.WpfClient
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : ObservableRecipient
     {
         public RestCollection<Chef> Chefs { get; set; }
 
-        public MainWindowViewModel() 
+        private Chef selectedChef;
+
+        public Chef SelectedChef
         {
-            Chefs = new RestCollection<Chef>("http://localhost:49326","chef");
+            get { return selectedChef; }
+            set {
+                SetProperty(ref selectedChef, value);
+                (DeleteChefCommand as RelayCommand).NotifyCanExecuteChanged();
+            }
+        }
+
+
+        public ICommand CreateChefCommand { get; set; }
+        public ICommand DeleteChefCommand { get; set; }
+        public ICommand UpdateChefCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Chefs = new RestCollection<Chef>("http://localhost:49326", "chef");
+
+            CreateChefCommand = new RelayCommand(() =>
+           {
+               Chefs.Add(new Chef()
+               {
+                   Name = "ASDASD"
+               });
+           });
+            DeleteChefCommand = new RelayCommand(() =>
+            {
+                Chefs.Delete(SelectedChef.ID);
+            },
+            () =>
+            {
+                return SelectedChef != null;
+            });
+
         }
     }
 }
